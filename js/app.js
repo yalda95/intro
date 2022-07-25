58dee07eb107ad08223619c22ae019b7e25f50e2
yalda <yalda.tsi@gmail.com>
2022-07-25 18:39:32 +0430

    header responsiveness fixed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 const hamburger = document.querySelector('.hamburger')
 const navbar = document.querySelector('.header-left_nav')
 const heroImage = document.querySelector('#hero-image')
+const closeBtn = document.querySelector('#close')
+
 const showNavbar = () =>{
-    navbar.display = "block"
+    navbar.style.display = "block"
+    closeBtn.style.display = "block"
     console.log(navbar)
 
+}
+
+const closeNavbar = () => {
+    navbar.style.display = "none"
 }
 
 function checkMediaQuery() {
@@ -13,13 +20,17 @@ function checkMediaQuery() {
       // Then log this message to the console
       console.log('Media Query Matched!')
       heroImage.src ="./images/image-hero-mobile.png"
+      
+
     }else if (window.innerWidth > 750){
         heroImage.src ="./images/image-hero-desktop.png"
-
-
+    }
+    if(window.innerWidth > 900) {
+        navbar.classList.toggle("block");
     }
   }
 
 hamburger.addEventListener('click',showNavbar)
+closeBtn.addEventListener('click',closeNavbar)
 window.addEventListener('resize',checkMediaQuery)
 checkMediaQuery();
